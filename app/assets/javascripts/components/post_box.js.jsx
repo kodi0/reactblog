a9d717ee46d0d8fea8b9c5b1d66c007c822acd0e
Roman <felixcon@gmail.com>
2014-12-29 19:20:36 +0300

    ajax version

--- app/assets/javascripts/components/post_box.js.jsx
+++ app/assets/javascripts/components/post_box.js.jsx
@@ -1,13 +1,33 @@
 var PostBox = React.createClass({
+  loadCommentsFromServer: function() {
+      $.ajax({
+        url: this.props.url,
+        dataType: 'json',
+        success: function(data) {
+          this.setState({data: data});
+        }.bind(this),
+        error: function(xhr, status, err) {
+          console.error(this.props.url, status, err.toString());
+        }.bind(this)
+      });
+    },
+  getInitialState: function() {
+        return {data: []};
+      },
+  componentDidMount: function() {
+      this.loadCommentsFromServer();
+    },    
   render: function() {
-    console.log(this.props.posts);
     return (
       <div className="postBox">
         <h2>Posts</h2>
         <PostForm />
         <hr />
-        <Posts data={this.props.posts} />
+        <Posts data={this.state.data} />
       </div>
     );
   }
 });
+React.render(
+  <PostBox url="/posts.json" />,
+  document.getElementById('post_box'));
